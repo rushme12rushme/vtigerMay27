673b93d72820e1ce64a8f1137d1ccdf60b7e8dd1
rushm <rushm@LAPTOP-DENRBFPO.mshome.net>
2024-06-07 09:20:53 +0530

    commented retry listener class

--- UtilityCreationMay27/src/test/java/testNGPractice/RetryListenerTest.java
+++ UtilityCreationMay27/src/test/java/testNGPractice/RetryListenerTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 import com.altius.crm.listenerutility.RetryListenerImplement;
 
 public class RetryListenerTest {
-@Test(retryAnalyzer=RetryListenerImplement.class)
+//@Test(retryAnalyzer=RetryListenerImplement.class)
 	public void retryMethod()
 	{
 		System.out.println("retry method");
